7e5b682b9453c6f728225cbbce113c18e6184709
MalavikkaSokkalingam <99123780+MalavikkaSokkalingam@users.noreply.github.com>
2022-03-07 17:26:22 +0530

    Add files via upload

---
+++ Core Java/Java11/Q2Java11.java
@@ -0,0 +1,17 @@
+package Java11;
+
+import java.util.ArrayList;
+class Q2Java11 {
+    public static void main(String[] args)
+    {
+        var<var> al = new ArrayList<>();
+            
+       
+        al.add(40);
+        al.add(80);
+        al.add(20);
+        
+        
+        System.out.println(al);
+    }
+}
