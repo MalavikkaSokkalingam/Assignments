5a0ca75f51770b35839ced59757e7e12107d10e6
MalavikkaSokkalingam <99123780+MalavikkaSokkalingam@users.noreply.github.com>
2022-04-04 16:34:24 +0530

    Add files via upload

---
+++ JavaScript/js basic assignment/Q2.js
@@ -0,0 +1,8 @@
+var name = window.prompt("Enter your Name");
+if(name=="Malavikka"||name=="Sokkalingam")
+{
+    alert("Welcome "+ name);
+}
+else{
+    alert("You're not Malavikka or Sokkalingam");
+}
